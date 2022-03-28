ad4ad7ed1097e393107dff805869bf48520091c2
Alex <holovkooleksii53@gmail.com>
2022-03-28 20:16:50 +0200

    initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,23 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
+
+  let discriminant = b**2 - 4 * a * c;
+
+  if (discriminant < 0){
+    arr = [];
+  }else if(discriminant === 0){
+    let root_0 = -b / (2 * a);
+    arr = [root_0];
+  }else{
+    let root_1 = (-b + Math.sqrt(discriminant) ) / (2 * a);
+    let root_2 = (-b - Math.sqrt(discriminant) ) / (2 * a);
+
+    arr = [root_1, root_2];
+  }
+
   return arr; // array
 }
 
