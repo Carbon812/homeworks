e61821d51d24628c5bdcfadf0b75eed62a50e8c4
Alex <holovkooleksii53@gmail.com>
2022-04-24 21:15:47 +0200

    arrays first commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,14 +1,41 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if(arr1.length !== arr2.length){
+    return result = false;
+  }
 
-  return result; // boolean
+  function isEven(arr1_item) {
+    // for (let i = 0; i < arr2.length; i++) {
+    //
+    // }
+
+    if(arr2[arr1.indexOf(arr1_item)] === arr1_item){
+      return true;
+    }
+  }
+
+  result = arr1.every(isEven);
+
+  return result;
+
 }
+
 
 function advancedFilter(arr) {
   let resultArr;
 
+  resultArr = arr.filter(function (element) {
+    return element > 0;
+  })
+
+  resultArr = resultArr.filter(function (element) {
+    return element % 3 === 0;
+  })
+
+  resultArr = resultArr.map(function (element) {
+    return element * 10;
+  })
   // Ваш код
 
   return resultArr; // array
