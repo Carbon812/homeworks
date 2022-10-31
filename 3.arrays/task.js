b1abb99804cd6139b00f313d457da0801f3696f5
Alex <holovkooleksii53@gmail.com>
2022-10-31 14:53:23 +0100

    second arr commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,9 +1,4 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  if(arr1.length !== arr2.length){
-    return false;
-  }
 
   function isEven(arr1_item, inx) {
 
@@ -13,28 +8,32 @@ function compareArrays(arr1, arr2) {
 
   }
 
-  result = arr1.every(isEven);
-
-  return result;
+  if(arr1.every(isEven) && arr1.length === arr2.length){
+    return true
+  }else{
+    return false
+  }
 
 }
 
 
 function advancedFilter(arr) {
+
   let resultArr;
 
-  resultArr = arr.filter(function (element) {
-    return element > 0;
-  })
+  // resultArr = arr.filter(function (element) {
+  //   return element > 0;
+  // })
+  //
+  // resultArr = resultArr.filter(function (element) {
+  //   return element % 3 === 0;
+  // })
+  //
+  // resultArr = resultArr.map(function (element) {
+  //   return element * 10;
+  // })
 
-  resultArr = resultArr.filter(function (element) {
-    return element % 3 === 0;
-  })
-
-  resultArr = resultArr.map(function (element) {
-    return element * 10;
-  })
-  // Ваш код
+  resultArr = arr.filter(elem => elem > 0).filter(elem => elem % 3 === 0).map(elem => elem * 10);
 
   return resultArr; // array
 }
