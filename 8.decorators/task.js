42891072a4676e61a0f4d65a6b486a28b4376586
Alex <holovkooleksii53@gmail.com>
2023-02-23 21:35:02 +0100

    first commit decorator

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,30 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = args.join(','); // получаем правильный хэш
+        let objectInCache = cache.find((item) => item.hash === hash); // ищем элемент, хэш которого равен нашему хэшу
+        if (objectInCache) { // если элемент не найден
+            console.log("Из кэша: " + objectInCache.value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+            return "Из кэша: " + objectInCache.value;
+        }
+
+        let result = func(...args); // в кэше результата нет - придётся считать
+        cache.push({hash: hash, value: result}) ; // добавляем элемент с правильной структурой
+        if (cache.length > 5) {
+            cache.shift() // если слишком много элементов в кэше надо удалить самый старый (первый)
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
+    return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
-}
-
-function debounceDecorator2(func) {
-  // Ваш код
-}
+// function debounceDecoratorNew(func) {
+//   // Ваш код
+// }
+//
+// function debounceDecorator2(func) {
+//   // Ваш код
+// }
