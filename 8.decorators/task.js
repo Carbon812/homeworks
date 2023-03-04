3df12ba2330e1ee89a3c75af97f7263c9a7f1ccc
Alex <holovkooleksii53@gmail.com>
2023-03-04 10:00:39 +0100

    commit task2

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,3 +1,5 @@
+//Task 1
+
 function cachingDecoratorNew(func){
     let cache = [];
 
@@ -20,12 +22,53 @@ function cachingDecoratorNew(func){
     return wrapper;
 }
 
+//Task 2
 
-// function debounceDecoratorNew(func) {
-//   // Ваш код
-// }
-//
+const sendSignal = (signalOrder, delaySignal) => console.log("Сигнал отправлен", signalOrder, delaySignal);
+const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
+
+setTimeout(() => upgradedSendSignal(1, 0)); // Сигнал отправлен + будет запланирован асинхронный запуск, который будет проигнорирован так как следующий сигнал отменит предыдущий (300 - 0 < 2000)
+setTimeout(() => upgradedSendSignal(2, 300), 300); // проигнорировано так как следующий сигнал отменит предыдущий (900 - 300 < 2000)
+setTimeout(() => upgradedSendSignal(3, 900), 900); // проигнорировано так как следующий сигнал отменит предыдущий (1200 - 900 < 2000)
+setTimeout(() => upgradedSendSignal(4, 1200), 1200); // проигнорировано так как следующий сигнал отменит предыдущий (2300 - 1200 < 2000)
+setTimeout(() => upgradedSendSignal(5, 2300), 2300); // Сигнал отправлен так как следующий вызов не успеет отменить текущий: 4400-2300=2100 (2100 > 2000)
+setTimeout(() => upgradedSendSignal(6, 4400), 4400); // проигнорировано так как следующий сигнал отменит предыдущий (4500 - 4400 < 2000)
+setTimeout(() => upgradedSendSignal(7, 4500), 4500); // Сигнал будет отправлен, так как последний вызов debounce декоратора (спустя 4500 + 2000 = 6500) 6,5с
+
+setTimeout(() => {
+    console.log(upgradedSendSignal.count); // было выполнено 3 отправки сигнала
+    console.log(upgradedSendSignal.allCount); // было выполнено 7 вызовов декорированной функции
+}, 7000);
+
+function debounceDecoratorNew(func, delay) {
+
+    let timeoutID = null;
+    function wrapper(...args) {
+        wrapper.allCount += 1;
+
+        if(timeoutID){
+            clearTimeout(timeoutID);
+        }
+        if(timeoutID === null){
+            setTimeout(() => {
+                (func(...args));
+                wrapper.count += 1;
+            }, 0);
+        }
+        timeoutID = setTimeout(() => {
+            (func(...args));
+            wrapper.count += 1;
+        }, delay);
+    }
+
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+
+    return wrapper;
+}
+
+
 // function debounceDecorator2(func) {
 //   // Ваш код
 // }
-let a;
+
